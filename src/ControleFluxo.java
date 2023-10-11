c81c47124223c849666ee4fe9a9b0d1f53511665
Luis Fernando <lfm.1383@outlook.com>
2023-10-11 18:01:32 -0300

    Controle Fluxo

---
+++ src/ControleFluxo.java
@@ -0,0 +1,29 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class ControleFluxo {
+    public static void main(String[] args) throws Exception {
+        try {
+            try (Scanner scanner = new Scanner(System.in)) {
+                System.out.println("Informe o primeiro parâmetro:");
+                int parameter1 = scanner.nextInt();
+                System.out.println("Informe o segundo parâmetro:");
+                int parameter2 = scanner.nextInt();
+                count(parameter1, parameter2);
+            }
+        } catch (ParametrosInvalidosException e) {
+            System.out.println(e.getMessage());
+        } catch (InputMismatchException e) {
+            System.out.println("Parametro inválido, informe um número inteiro.");
+        }
+    }
+
+    static void count(int p1, int p2) throws ParametrosInvalidosException{
+        int iteraction = p2 - p1;
+        for (int i = 1; i <= iteraction; i++) {
+            System.out.println("Imprimindo o número "+i);
+        }
+        if(iteraction < 0)
+            throw new ParametrosInvalidosException();
+    }
+}
---
+++ src/ParametrosInvalidosException.java
@@ -0,0 +1,5 @@
+public class ParametrosInvalidosException extends RuntimeException {
+  ParametrosInvalidosException() {
+    super("O segundo par\u00E2metro deve ser maior que o primeiro.");
+  }
+}
